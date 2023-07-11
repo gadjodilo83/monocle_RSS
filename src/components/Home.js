0bc14d79bf6f89e1291e212a64a59f47aa02ff05
gadjodilo83 <gadjodilo1983@gmail.com>
2023-07-11 20:23:39 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -56,30 +56,29 @@ const Home = () => {
 
 const fetchGpt = async () => {
   const messages = [
-    { "role": "system", "content": systemPrompt },
-    { "role": "user", "content": window.transcript },
+    { role: "system", content: systemPrompt },
+    { role: "user", content: window.transcript },
   ];
 
-	const response = await fetch(`https://api.openai.com/v1/chat/completions`, {
-	  body: JSON.stringify({
-		model: "gpt-3.5-turbo",
-		messages: messages,
-		temperature: temperature,
-		max_tokens: 2000,
-	  }),
-	  headers: {
-		Authorization: `Bearer ${apiKey}`,
-		"Content-Type": "application/json",
-	  },
-	  method: "POST",
-	});
+  const response = await fetch(`https://api.openai.com/v1/chat/completions`, {
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: messages,
+      temperature: temperature,
+      max_tokens: 2000,
+    }),
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+  });
 
-	if (!response.ok) {
-	  const message = await response.text();
-	  console.error('API request error:', response.status, message);
-	  throw new Error(`API request failed: ${message}`);
-	}
-
+  if (!response.ok) {
+    const message = await response.text();
+    console.error("API request error:", response.status, message);
+    throw new Error(`API request failed: ${message}`);
+  }
 
   const resJson = await response.json();
   const res = resJson?.choices?.[0]?.message?.content;
