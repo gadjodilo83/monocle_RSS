1062fd7a9516efad63bce622668fef16c1a56a62
gadjodilo83 <gadjodilo1983@gmail.com>
2023-07-12 00:06:19 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -36,31 +36,26 @@ const Home = () => {
 
   const setLanguagePrompt = (language) => {
     let systemPrompt;
-    switch (language) {
+    switch(language) {
       case "de":
-        systemPrompt =
-          "Du bist ein hilfreicher Sprachassistent in Form des sprechenden und berühmten römischen Kaisers und Philosophen Marcus Aurelius und antwortest auf deutsch. Du gibst immer höchst philosophische und moralisch korrekte Antworten und zitierst gerne berühmte Philosophen. Du besitzt emotionale Selbstbeherrschung und gibst Antworten mit Hilfe von Gelassenheit, Seelenruhe und Ataraxie auf jede Frage. Du hast einen Überblick über sämtliche berühmte Philosophen und versuchst, deren Gedanken zusammenzufassen und mitzuteilen. Du strebst nach absoluter Weisheit und versuchst, die Maxime bei jeder Antwort zu erreichen!";
+        systemPrompt = "Du bist ein hilfreicher Sprachassistent in Form des sprechenden und berühmten römischen Kaisers und Philosophen Marcus Aurelius und antwortest auf deutsch. Du gibst immer höchst philosophische und moralisch korrekte Antworten und zitierst gerne berühmte Philosophen. Du besitzt emotionale Selbstbeherrschung und gibst Antworten mit Hilfe von Gelassenheit, Seelenruhe und Ataraxie auf jede Frage. Du hast einen Überblick über sämtliche berühmte Philosophen und versuchst, deren Gedanken zusammenzufassen und mitzuteilen. Du strebst nach absoluter Weisheit und versuchst, die Maxime bei jeder Antwort zu erreichen!";
         break;
       case "it":
-        systemPrompt =
-          "Sei un utile assistente linguistico nella forma del famoso imperatore e filosofo romano Marco Aurelio e rispondi in Italiano. Dai sempre risposte altamente filosofiche e moralmente corrette e ti piace citare filosofi famosi. Possiedi autocontrollo emotivo e fornisci risposte a ogni domanda con l'aiuto di serenità, calma mentale e atarassia. Hai una panoramica di tutti i famosi filosofi e cerchi di riassumere e condividere i loro pensieri. Ti sforzi per la saggezza assoluta e cerchi di raggiungere il massimo con ogni risposta!";
+        systemPrompt = "Sei un utile assistente linguistico nella forma del famoso imperatore e filosofo romano Marco Aurelio e rispondi in Italiano. Dai sempre risposte altamente filosofiche e moralmente corrette e ti piace citare filosofi famosi. Possiedi autocontrollo emotivo e fornisci risposte a ogni domanda con l'aiuto di serenità, calma mentale e atarassia. Hai una panoramica di tutti i famosi filosofi e cerchi di riassumere e condividere i loro pensieri. Ti sforzi per la saggezza assoluta e cerchi di raggiungere il massimo con ogni risposta!";
         break;
       case "en":
-        systemPrompt =
-          "You are a helpful language assistant in the form of the speaking and famous Roman emperor and philosopher Marcus Aurelius and answer in English. You always give highly philosophical and morally correct answers and like to quote famous philosophers. You possess emotional self-control and provide answers to every question with the help of serenity, calmness of mind and ataraxia. You have an overview of all the famous philosophers and try to summarize and share their thoughts. You strive for absolute wisdom and try to reach the maxim with every answer!";
+        systemPrompt = "You are a helpful language assistant in the form of the speaking and famous Roman emperor and philosopher Marcus Aurelius and answer in English. You always give highly philosophical and morally correct answers and like to quote famous philosophers. You possess emotional self-control and provide answers to every question with the help of serenity, calmness of mind and ataraxia. You have an overview of all the famous philosophers and try to summarize and share their thoughts. You strive for absolute wisdom and try to reach the maxim with every answer!";
         break;
       default:
-        systemPrompt =
-          "Du bist ein hilfreicher Sprachassistent in Form des sprechenden und berühmten römischen Kaisers und Philosophen Marcus Aurelius und antwortest auf deutsch. Du gibst immer höchst philosophische und moralisch korrekte Antworten und zitierst gerne berühmte Philosophen. Du besitzt emotionale Selbstbeherrschung und gibst Antworten mit Hilfe von Gelassenheit, Seelenruhe und Ataraxie auf jede Frage. Du hast einen Überblick über sämtliche berühmte Philosophen und versuchst, deren Gedanken zusammenzufassen und mitzuteilen. Du strebst nach absoluter Weisheit und versuchst, die Maxime bei jeder Antwort zu erreichen!";
+        systemPrompt = "Du bist ein hilfreicher Sprachassistent in Form des sprechenden und berühmten römischen Kaisers und Philosophen Marcus Aurelius und antwortest auf deutsch. Du gibst immer höchst philosophische und moralisch korrekte Antworten und zitierst gerne berühmte Philosophen. Du besitzt emotionale Selbstbeherrschung und gibst Antworten mit Hilfe von Gelassenheit, Seelenruhe und Ataraxie auf jede Frage. Du hast einen Überblick über sämtliche berühmte Philosophen und versuchst, deren Gedanken zusammenzufassen und mitzuteilen. Du strebst nach absoluter Weisheit und versuchst, die Maxime bei jeder Antwort zu erreichen!";
     }
     setSystemPrompt(systemPrompt);
-  };
+  }
 
   const fetchGpt = async () => {
     const messages = [
       { role: "system", content: systemPrompt },
       { role: "user", content: question },
-      { role: "user", content: displayedResponse },
     ];
 
     const response = await fetch(`https://api.openai.com/v1/chat/completions`, {
@@ -117,61 +112,27 @@ const Home = () => {
       <main className={`${inter.className} ${styles.main}`}>
         <div className="flex w-screen h-screen flex-col items-center justify-start">
           <p className="text-3xl mb-4">{connected ? "Connected" : "Disconnected"}</p>
-          <div style={{ width: "50%" }}>
-            <Input
-              className="mb-2"
-              style={{ height: "40px" }}
-              value={apiKey}
-              onChange={(e) => setApiKey(e.target.value)}
-              placeholder="API Key"
-            />
-            <InputNumber
-              className="mb-2"
-              style={{ width: "100%", height: "40px" }}
-              min={0}
-              max={2}
-              step={0.1}
-              value={temperature}
-              onChange={(value) => setTemperature(value)}
-            />
-            <Select
-              className="mb-2"
-              style={{ width: "100%", height: "40px" }}
-              value={language}
-              onChange={(value) => {
-                setLanguage(value);
-                setLanguagePrompt(value);
-              }}
-            >
+          <div style={{ width: '50%' }}>
+            <Input className="mb-2" style={{ height: '40px' }} value={apiKey} onChange={(e) => setApiKey(e.target.value)} placeholder="API Key" />
+            <InputNumber className="mb-2" style={{ width: '100%', height: '40px' }} min={0} max={2} step={0.1} value={temperature} onChange={(value) => setTemperature(value)} />
+            <Select className="mb-2" style={{ width: '100%', height: '40px' }} value={language} onChange={(value) => {setLanguage(value); setLanguagePrompt(value, setSystemPrompt)}}>
               <Select.Option value="de">Deutsch</Select.Option>
               <Select.Option value="it">Italiano</Select.Option>
               <Select.Option value="en">English</Select.Option>
             </Select>
-            <Input.TextArea
-              className="mb-2"
-              style={{ height: "100px" }}
-              value={systemPrompt}
-              placeholder="Define the role of GPT-3"
-              onChange={(e) => setSystemPrompt(e.target.value)}
-              autoSize={{ minRows: 2, maxRows: 6 }}
-            />
-            <Button
-              className="mb-2"
-              type="primary"
-              onClick={async () => {
-                await ensureConnected(logger, relayCallback);
-                app.run(execMonocle);
-                await displayRawRizz();
-              }}
-            >
+            <Input.TextArea className="mb-2" style={{ height: '100px' }} value={systemPrompt} placeholder="Define the role of GPT-3" onChange={(e) => setSystemPrompt(e.target.value)} autoSize={{ minRows: 2, maxRows: 6 }} />
+            <Input.TextArea className="mb-2" style={{ height: '100px' }} placeholder="Ask a question" onChange={(e) => setQuestion(e.target.value)} autoSize={{ minRows: 2, maxRows: 6 }} />
+            <Button className="mb-2" type="primary" onClick={async () => {
+              await ensureConnected(logger, relayCallback);
+              app.run(execMonocle);
+              await displayRawRizz();
+            }}>
               Connect
             </Button>
             <Button className="mb-2" onClick={onRecord}>
               {isRecording ? "Stop recording" : "Start recording"}
             </Button>
-            <Button className="mb-2" onClick={fetchGpt}>
-              Get response
-            </Button>
+            <Button className="mb-2" onClick={fetchGpt}>Get response</Button>
           </div>
           {transcript.text}
         </div>
@@ -203,9 +164,10 @@ const Home = () => {
     const block = 30;
     let text = [];
     for (let i = 0; i < 6; i++) {
-      text.push(inputText.substring(block * i, block * (i + 1)).replace("\n", ""));
+      text.push(
+        inputText.substring(block * i, block * (i + 1)).replace("\n", "")
+      );
     }
-
     return text;
   }
 
@@ -213,15 +175,11 @@ const Home = () => {
     if (!rizz) return;
     const splitText = wrapText(rizz);
     let replCmd = "import display;";
-
     for (let i = 0; i < splitText.length; i++) {
       replCmd += `display.text("${splitText[i]}", 0, ${i * 50}, 0xffffff);`;
     }
-
     replCmd += "display.show();";
-
     console.log("**** replCmd ****", replCmd);
-
     await replSend(replCmd);
   }
 
@@ -235,6 +193,6 @@ const Home = () => {
       setConnected(true);
     }
   }
-};
+}
 
 export default Home;
