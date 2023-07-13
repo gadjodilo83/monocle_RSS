c377bbca2e4aaae90b1d140a0366bc9aba141ae7
gadjodilo83 <gadjodilo1983@gmail.com>
2023-07-13 17:45:33 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -177,7 +177,7 @@ async function displayRizz(rizz) {
   let texts = [];
   for (let i = 0; i < splitText.length; i++) {
     let textObjectName = `t${i}`;
-    replCmd += `${textObjectName} = display.Text("${splitText[i]}", 0, ${i * 50}, 0xffffff)\n`;
+    replCmd += `${textObjectName} = display.Text("${splitText[i]}", -30, ${i * 50}, 0xffffff)\n`;
     texts.push(textObjectName);
     await new Promise(resolve => setTimeout(resolve, 1000));
   }
