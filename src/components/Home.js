4a83a6974782a24ca641fbc257cf101e03e7143a
gadjodilo83 <gadjodilo1983@gmail.com>
2023-07-19 17:26:02 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -71,7 +71,7 @@ const Home = () => {
         model: "gpt-3.5-turbo",
         messages: messages,
         temperature: temperature,
-        max_tokens: 250,
+        max_tokens: 500,
       }),
       headers: {
         Authorization: `Bearer ${apiKey}`,
