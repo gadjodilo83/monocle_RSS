0faff0c09caa178aaa690bcb7b35a1eedbd520af
gadjodilo83 <gadjodilo1983@gmail.com>
2023-08-03 13:21:11 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -76,28 +76,29 @@ const Home = () => {
     },
   });
 
-  const displayRizz = useCallback(async (rizz) => {
-    if (!rizz) return;
+const displayRizz = useCallback(async (rizz) => {
+  if (!rizz) return;
 
-    const splitText = wrapText(rizz);
-    const groupSize = 4;
+  const splitText = wrapText(rizz);
+  const groupSize = 4;
 
-    for (let i = 0; i < splitText.length; i += groupSize) {
-      const group = splitText.slice(i, i + groupSize);
-      const textCmds = group.map((text, index) => {
-        const xCoordinate = 0; // Beispielwert für die x-Koordinate
-        const yCoordinate = index * 50; // Zeilen t1 bis t4
-        return `display.Text('${cleanText(text.replace(/"/g, ""))}', ${xCoordinate}, ${yCoordinate}, display.WHITE)`;
-      });
+  for (let i = 0; i < splitText.length; i += groupSize) {
+    const group = splitText.slice(i, i + groupSize);
+    const textCmds = group.map((text, index) => {
+      const xCoordinate = 0; // Beispielwert für die x-Koordinate
+      const yCoordinate = index * 50; // Zeilen t1 bis t4
+      return `display.Text('${cleanText(text.replace(/"/g, ""))}', ${xCoordinate}, ${yCoordinate}, display.WHITE)`;
+    });
 
-      const textCmd = `display.show([${textCmds.join(", ")}])`;
-      const clearCmd = "display.clear()";
+    const textCmd = `display.show([${textCmds.join(", ")}])`;
+    const clearCmd = "display.clear()";
 
-      await delay(100); // 2.5 Sekunden warten
-      await replSend(`${clearCmd}\n${textCmd}\n`); // clear() und display.show senden
-      await delay(6000); // 2.5 Sekunden warten
+    await delay(100); // 2.5 Sekunden warten
+    await replSend(`${clearCmd}\n${textCmd}\n`); // clear() und display.show senden
+    await delay(6000); // 2.5 Sekunden warten
 
-} []);
+  }
+}, []);
 
   const displayRawRizz = useCallback(async (rizz) => {
     await replRawMode(true);
